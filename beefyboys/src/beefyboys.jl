82cc302ef48c114ba73b6a299f18f7b556f0546f
ehasselwander <61474575+ehasselwander@users.noreply.github.com>
2020-03-02 21:02:22 -0500

    Added 'findClosestNode' formula

--- beefyboys/src/beefyboys.jl
+++ beefyboys/src/beefyboys.jl
@@ -63,6 +63,47 @@ function test(x,y,z,theta)
     return (f1,f2)
 end
 
+
+function findClosestNode(Master,sampleAsHomogenous)
+
+	# Master is an Nx2 array. Col. 1 is 4x4 nodes, Col. 2 is node's row in 'Master'
+
+	nodeListLength = size(Master,1)
+	rollingMin = 1
+	#closestNode = [0 0 0]
+	minRow = 5000
+    sample = transpose(sampleAsHomogenous[1:3,4])
+
+	for i in 1:nodeListLength
+		
+	currentDistance=dist(sampleAsHomogenous, Master[i][1])	
+		
+		
+	#currentDistance = sqrt((sample[1,1]-Master[i][1][1,4])^2 + (sample[1,2]-Master[i][1][2,4])^2 + (sample[1,3]-Master[i][1][3,4])^2)
+	#only linear distance	
+		
+		if i == 1
+			rollingMin = currentDistance
+			#closestNode = Master[i,:]
+			minRow = 1
+		end
+	
+		if currentDistance < rollingMin
+			rollingMin = currentDistance
+			#closestNode = Master[i,:]
+			minRow = i
+		end
+
+	end 
+
+	#println("The minimum distance is ", rollingMin)
+	#println("To the point ", closestNode)
+	#println("This point is row ", minRow," in the Node List")
+return minRow
+
+end
+
+
 function prm(s,g,O)
     limits=world(s,g,O) #max and mins of objects in our world
     step = .5*minimum([O[:,4];1]); #set step to half the minimum radius

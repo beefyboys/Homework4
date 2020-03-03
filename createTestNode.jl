09a4fe92f837ea26b486564f1daef892601e1232
Jrmikulec <61474389+Jrmikulec@users.noreply.github.com>
2020-03-03 10:48:21 -0500

    Update createTestNode.jl
    rewritten

--- createTestNode.jl
+++ createTestNode.jl
@@ -1,13 +1,18 @@
 module createTestNode
 
-function findTestNode(closestNode, sample, stepSize)
-
-       vectorBetween = sample - closestNode
-
-       norm = sqrt(vectorBetween[1,1]^2 + vectorBetween[1,2]^2 + vectorBetween[1,3]^2)
-
-       testNode = (vectorBetween/norm) * stepSize
-
-       end
+function findTestNode(closestNode, sample1, stepSize)
+       travel = zeros(3,1)
+       testNode = zeros(4,4)
+       travel = sample1[1:3,4] - closestNode[1:3,4]
+       norm = sqrt((sample1[1,4]-closestNode[1,4])^2 + (sample1[1,4]-closestNode[1,4])^2 + (sample1[1,4]-closestNode[1,4])^2)
+       
+       normWithStep = (travel/norm) * stepSize
+       
+       testNode[1:3,4] = testNode[1:3,4] + normWithStep
+       
+       testNode[1:3,1:3] = sample1[1:3,1:3]
+     
+       testNode[4,4] = 1
+       
 
 end # module

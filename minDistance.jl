f27e80f593d0aa9723bc3c469fcfc368a21ff29a
Jrmikulec <61474389+Jrmikulec@users.noreply.github.com>
2020-03-02 20:42:07 -0500

    Update minDistance.jl
    Made findClosestNode take 4x4s as its inputs

--- minDistance.jl
+++ minDistance.jl
@@ -3,36 +3,37 @@ module minDistance
 greet() = print("Hello Loser!")
 
 
-function findMin(nodeList,sample)
+function findClosestNode(Master,sampleAsHomogenous)
 
 	#nodeList should be a N x 3 list of points in 3space, sample should be a single point in 3space
 
-	nodeListLength = size(nodeList,1)
+	nodeListLength = size(Master,1)
 	rollingMin = 1
 	closestNode = [0 0 0]
 	minRow = 5000
+    sample = transpose(sampleAsHomogenous[1:3,4])
 
 	for i in 1:nodeListLength
-		currentDistance = sqrt((sample[1,1]-nodeList[i,1])^2 + (sample[1,2]-nodeList[i,2])^2 + (sample[1,3]-nodeList[i,3])^2)
+		currentDistance = sqrt((sample[1,1]-Master[i][1][1,4])^2 + (sample[1,2]-Master[i][1][2,4])^2 + (sample[1,3]-Master[i][1][3,4])^2)
 
 		if i == 1
 			rollingMin = currentDistance
-			closestNode = nodeList[i,:]
+			closestNode = Master[i,:]
 			minRow = 1
 		end
 	
 		if currentDistance < rollingMin
 			rollingMin = currentDistance
-			closestNode = nodeList[i,:]
+			closestNode = Master[i,:]
 			minRow = i
 		end
 
 	end 
 
-	println("The minimum distance is ", rollingMin)
-	println("To the point ", closestNode)
-	println("This point is row ", minRow," in the Node List")
-
+	#println("The minimum distance is ", rollingMin)
+	#println("To the point ", closestNode)
+	#println("This point is row ", minRow," in the Node List")
+return minRow
 
 end
 

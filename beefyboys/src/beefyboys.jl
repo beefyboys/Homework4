ef2dec5844a96c3ed9d8f5929dcd08cd66d5a042
dcho228 <61474524+dcho228@users.noreply.github.com>
2020-03-02 21:03:23 -0500

    added dist function

--- beefyboys/src/beefyboys.jl
+++ beefyboys/src/beefyboys.jl
@@ -63,6 +63,28 @@ function test(x,y,z,theta)
     return (f1,f2)
 end
 
+function dist(f1, f2)
+
+r_1 = f1[1:3,1:3];
+r_2 = f2[1:3,1:3];
+
+x_1 = f1[1,4];
+x_2 = f1[2,4];
+x_3 = f1[3,4];
+
+y_1 = f2[1,4];
+y_2 = f2[2,4];
+y_3 = f2[3,4];
+
+R = r_1*transpose(r_2);
+theta = acos((tr(R)-1)/2);
+
+dlinear = sqrt((y_1-x_1)^2 + (y_2-x_2)^2 + (y_3-x_3)^2);
+
+println("d =")
+return theta*dlinear
+
+end 
 
 function findClosestNode(Master,sampleAsHomogenous)
 
